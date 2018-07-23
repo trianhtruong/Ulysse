9fa5c5eb3bf191838e181ec7079a497d1d4fd93d
trianhtruong <41551153+trianhtruong@users.noreply.github.com>
2018-07-23 03:04:35 +0200

    Update ulysse.py

--- ulysse.py
+++ ulysse.py
@@ -5,23 +5,12 @@ from bs4 import BeautifulSoup
 import urllib.request
 import basictools
 
-
-# Test sur get_text()
-def test_sur_get_text(url):
-	with open("result_test_get_text.txt", "w") as file:
-		html = urllib.request.urlopen(url)
-		bsObj = BeautifulSoup(html, "lxml")
-		result = bsObj.get_text()
-		file.write(result)
-
 def main():
 	url = input("Enter the starting url: ")
 	basictools.getRobotsTxt(url)
 	basictools.getLinksFromUrlAndAddHttp(url)
 	basictools.download_html(url)
 	basictools.download_html_as_txt(url)
-#	test_sur_get_text(myUrl)
-	
 
 main()
 
