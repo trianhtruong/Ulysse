2666929a313a65aa4bc8e7ffad9f46f6803a5ab0
trianhtruong <41551153+trianhtruong@users.noreply.github.com>
2018-07-29 07:27:28 +0200

    Update ulysse.py

--- ulysse.py
+++ ulysse.py
@@ -1,72 +1,84 @@
 # Ulysse v0.1
-# Let's go !
 
 from bs4 import BeautifulSoup
 import urllib.request
 import basictools
-
+import menu
 
 def erase(file):
 	with open(file, "w") as f:
 		f.write("")
 
-# This function get the first link inside 'links.txt' - it's the next link to
-# crawl. Then, remove it from the 'links.txt' file so that the second line
-# become the first and so on.
+# This function get the first link inside 'links.txt' then, remove it from
+# the 'links.txt' file so that the second line become the first and so on.
 def crawl(file):
 	with open(file, "r") as f1:
-		# Use the iterator to get the first line in the file.
-		# The iterator is now after the first line.
+# Use the iterator to get the first line in the file. The iterator is now 
+# after the first line.
 		urltocrawl = next(f1)
 		with open("links.txt.temp", "w") as f2:
-			# Read from the iterator position, therefore ignoring the first
-			# line and save what's read into a temp file.
+			# Read from the iterator position, ignoring the first line.
 			f2.write(f1.read())
-	# Simply overwrite the first file with the temp file. Now all line have 
-	# gone one line up and the first line is deleted (it's in 'urltocrawl')
+# Overwrite the first file with the temp file. Now all line have 
+# gone one line up and the first line is deleted (it's in 'urltocrawl')
 	with open("links.txt.temp", "r") as f2:
 		with open(file, "w") as f1:
 			for line in f2:
 				f1.write(line)
-	# We keep logging visited urls. See lines 54 / 55.
+
 	with open("visited.txt", "a") as v:
 		v.write(urltocrawl)			
-	return urltocrawl	
+	return urltocrawl
 
-# Used to separate the links obtained from different pages in 'links.txt'
 def drawline(file):
 	with open(file, "a") as f:
 		f.write("-------------------------------\n")
 
-def main():
-	# Start by erasing the old links from previous crawls.
-	# Save them if you want to keep them.
+def initialize():
 	erase("links.txt")
 	erase("links.txt.temp")
 	erase("visited.txt")
 
-	url = input("Enter the starting url: ")
-	depth = int(input("How many links do you want to crawl ?:"))
+	url = input("\n" + "Enter the starting url: ")
+	depth = int(input("\n" + "How many links do you want to crawl ?: "))
 
-	# saving the value of the starting url to transform relative urls in
-	# absolute ones.
-	# Only work as long as the crawler stays on the same domain...
+# saving the value of the starting url to transform relative urls in
+# absolute ones. Only works as long as the crawler stays on the same domain
 	domain = url
+	with open('domain.txt', 'w') as d:
+		d.write(domain)
+	return url, depth, domain
 
-	# Get the links from the starting page and save them inside "links.txt"
-	print("Visiting " + url)
-	basictools.getLinksFromUrlAndAddHttp(url, domain)
+def visitFirstPage(url, domain):
+# Get the links from the starting page and save them inside 'links.txt'
+		print("\n" + "Visiting " + url + "\n")
+		basictools.getLinksFromUrlAndAddHttp(url, domain)
 
-	# "visited.txt" serves as a log of visited links.
-	# The number of visited links should be equal to the value of 'depth'
-	with open("visited.txt", "a") as v:
-		v.write(url)
-	depth -=1
+		# "visited.txt" serves as a log of visited links.
+		# The number of visited links should be equal to the value of 'depth'
+		with open("visited.txt", "a") as v:
+			v.write(url)
+
+def main():
+	choice = menu.display()
+	
+# New crawl. Start by erasing the old files.	
+	if choice == '1':
+		url, depth, domain = initialize()
+		visitFirstPage(url, domain)
+		depth -=1
+
+	elif choice == '2':
+		with open('domain.txt', 'r') as d:
+			domain = d.readline()
+		print ("Domain : " + domain)
+		depth = int(input("\n" + "How many links do you want to crawl ?: "))
+	
+	else :
+		print("Something wrong happened.")
 
 	while(depth > 0):
-		# Serves as a visual limit beetwen the gathered links
-		# So we can tell which links come from which url a little easier
-		# Can be commented out if the user has no use for that.
+		# Serve as a separator between visited pages.
 		drawline("links.txt")
 
 		# The part that allow the crawler to explore.
@@ -75,16 +87,12 @@ def main():
 		basictools.getLinksFromUrlAndAddHttp(url, domain)
 		depth -=1
 
-		# Additionnal capacities. Just uncomment if you need to download html
-		# grab the robots.txt or something else.
-		# Careful for now. The errors are not handled yet.
+		# Additionnal capacities. Just uncomment to use.
+		# Errors not handled, yet.
 
 		#basictools.getRobotsTxt(url)
 		#basictools.getLinksFromUrlAndAddHttp(url)
 		#basictools.download_html(url)
 		#basictools.download_html_as_txt(url)
 
-
 main()
-
-
